747fb628eead8a2e2f4a7369b9a6a902f9d6ec8e
Adil_Majhoul <itsadilhadid@gmail.com>
2024-04-17 15:19:56 +0100

    ok we passed anti boting system successfully now how to speed up puppeteer using different techniques other than navigation which is slow

--- src/learningPuppeteer/crunchbase.js
+++ src/learningPuppeteer/crunchbase.js
@@ -1,27 +1,76 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { executablePath } from 'puppeteer';
 
+async function login() {
+  puppeteer.use(StealthPlugin());
+
+  const browser = await puppeteer.launch({
+    headless: false, // Change to true if you want to run in headless mode
+    args: ['--no-sandbox'],
+    ignoreHTTPSErrors: true,
+    executablePath: executablePath(),
+    timeout: 30000, // Set a timeout for browser launch (in milliseconds)
+  });
+
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(0);
+  const loginUrl = 'https://www.crunchbase.com/login';
+
+  //   const username = process.env.USERNAME;
+  //   const password = process.env.PASSWORD;
+
+  await page.goto(loginUrl);
+
+  await new Promise((_func) => setTimeout(_func, 2000));
+
+  //   await page.type('#mat-input-0', username);
+  //   await page.type('#mat-input-1', password);
+  await page.type('#mat-input-0', 'adil2mae@gmail.com');
+  await page.type('#mat-input-1', 'Crunchbaseskhon1-');
+
+  await new Promise((_func) => setTimeout(_func, 2000));
+
+  console.log('Login');
+
+  const logingButton =
+    '#mat-tab-content-0-0 > div > login > form > div.actions > button.login.mdc-button.mdc-button--raised.mat-mdc-raised-button.mat-primary.mat-mdc-button-base';
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    page.click(logingButton),
+  ]);
+
+  await new Promise((_func) => setTimeout(_func, 5000));
+
+  return { browser, page };
+}
+
+// -----------------------------
 export async function randomWait(fixedMs, randomdMs) {
   let ms = Math.floor(Math.random() * randomdMs);
   ms = ms + fixedMs;
 
   await new Promise((_func) => setTimeout(_func, ms));
 }
+// --------------------------------
+
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const { browser, page } = await login();
+
+  await randomWait(1500, 500);
 
   // Navigate to login page
   await page.goto('https://www.crunchbase.com/login', {
     waitUntil: 'networkidle0',
   });
 
+  await randomWait(1500, 500);
+
   // Enter credentials and login
-  await page.type('#mat-input-0', 'adil2mae@gmail.com');
-  await page.type('#mat-input-1', 'Crunchbaseskhon1-');
-  await Promise.all([
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
-    page.click('.login-btn'),
-  ]);
+
+  await randomWait(1500, 500);
+
+  await randomWait(1500, 500);
 
   // Navigate to OpenAI's company page
   await page.goto(
@@ -29,10 +78,14 @@ export async function randomWait(fixedMs, randomdMs) {
     { waitUntil: 'networkidle0' },
   );
 
+  await randomWait(1500, 500);
+
   // Wait for the desired element to appear on the page
   await page.waitForSelector(
     '#mat-tab-nav-panel-0 > div > div > page-centered-layout.overview-divider.ng-star-inserted > div > row-card > div > div:nth-child(1) > profile-section > section-card > mat-card > div.section-content-wrapper > anchored-values > div:nth-child(6) > a > div > field-formatter > span',
   );
+
+  await randomWait(1500, 500);
 
   // Get the HTML and parse it using a query selector
   const html = await page.evaluate(() => {
